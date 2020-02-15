109c6f65b62c2c3940efc50a1579a185a96f00d0
Alex Kostas <k.alexandros299@gmail.com>
2020-02-16 00:25:31 +0200

    Add basic expression-handling parser

--- src/com/compiler/MyLang/MyLang.java
+++ src/com/compiler/MyLang/MyLang.java
@@ -41,7 +41,12 @@ public class MyLang {
         var tokens = scanner.scanTokens();
 
         //TODO: Temporarily just printing all the tokens
-        for(Token token : tokens) System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message){
@@ -51,5 +56,10 @@ public class MyLang {
     static void report(int line, String where, String message){
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF) report(token.line, " at end", message);
+        else report(token.line, " at" + token.lexeme + "'", message);
     }
 }
