c5fbbcc4fa49ed2e506af2cbaf41e2c75c669725
Alex Kostas <k.alexandros299@gmail.com>
2020-02-17 20:19:58 +0200

    Add support for variable assignment

--- src/com/compiler/MyLang/Interpreter.java
+++ src/com/compiler/MyLang/Interpreter.java
@@ -3,6 +3,7 @@ package com.compiler.MyLang;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements){
         try{
@@ -85,6 +86,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitVariableExpr(Expr.Variable expr){
+        return environment.get(expr.name);
+    }
+
+    @Override
     public Void visitExpressionStmt(Stmt.Expression stmt){
         evaluate(stmt.expression);
         return null;
@@ -95,6 +101,23 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         Object value = evaluate(stmt.expression);
         System.out.println(makeString(value));
         return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt){
+        Object value = null;
+        if(stmt.initializer != null)
+            value = evaluate(stmt.initializer);
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
     private void checkNumberOperand(Token operator, Object operand){
